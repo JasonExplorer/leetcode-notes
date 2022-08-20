0a989fc460ae154667ad988bbebf7cc1ea838d51
xixibb <liu_yichun@163.com>
2022-08-20 12:07:14 +0800

    二分查找

--- src/main/java/leecode200/SingleElementInASortedArray.java
+++ src/main/java/leecode200/SingleElementInASortedArray.java
@@ -29,7 +29,7 @@ public class SingleElementInASortedArray {
     public int singleNonDuplicate(int[] nums) {
 
         int left = 0 , right = nums.length -1;
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left)/2;
             if (mid % 2 == 1) {
                 mid--;
