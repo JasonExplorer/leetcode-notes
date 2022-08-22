ffc2e8874b21adf582c1585b6e99b4dfcd553607
xixibb <liu_yichun@163.com>
2022-08-22 08:42:18 +0800

    链表

--- src/main/java/leecode200/ReverseLinkedList.java
+++ src/main/java/leecode200/ReverseLinkedList.java
@@ -1,5 +1,7 @@
 package leecode200;
 
+import common.ListNode;
+
 /**
  * @author xixibb
  * @date 2022/08/21
@@ -35,13 +37,6 @@ public class ReverseLinkedList {
         }
         return pre;
 
-    }
-    public class ListNode {
-        int val;
-        ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
 }
