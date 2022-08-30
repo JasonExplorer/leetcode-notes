1b88facc461ee345eb8620d4727cf7ea9c9ab38d
xixibb <liu_yichun@163.com>
2022-08-30 15:02:38 +0800

    64. Minimum Path Sum (Medium)

---
+++ src/main/java/leecode200/MinimumPathSum.java
@@ -0,0 +1,51 @@
+package leecode200;
+
+/**
+ * @author xixibb
+ * @date 2022/08/27
+ * @desprication 64. Minimum Path Sum (Medium)
+ */
+public class MinimumPathSum {
+
+    /**
+     * 64. 最小路径和
+     *
+     * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+     *
+     * 说明：每次只能向下或者向右移动一步。
+     *
+     * 示例 1：
+     * 输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+     * 输出：7
+     * 解释：因为路径 1→3→1→1→1 的总和最小
+     *
+     * 示例 2：
+     * 输入：grid = [[1,2,3],[4,5,6]]
+     * 输出：12
+     */
+    public int minPathSum(int[][] grid) {
+
+        if (grid == null || grid.length == 0|| grid[0].length == 0) {
+            return 0;
+        }
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] dp = new int[rows][cols];
+        // 特殊情况，只有一行或者只有一列dp就是前一个路径+ grid
+        for (int i =1; i < rows; i++) {
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for (int j =1; j < cols; j++) {
+            dp[0][j] = dp[0][j-1] + grid[0][j];
+        }
+
+        // dp[i][j] 智能上下或者左右， dp[i-1][j] 或者 dp[i][j-1] 两种移动方式， 求其中的最大值
+        for (int i = 1; i < rows; i++) {
+            for (int j=1; j < cols; j++) {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+            }
+        }
+        return dp[rows -1][cols -1];
+    }
+}
