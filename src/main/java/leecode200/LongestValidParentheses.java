87c75aa0f4b4fd20d7cddc746087f4c29be30783
xixibb <liu_yichun@163.com>
2022-09-05 16:27:07 +0800

    32. Longest Valid Parentheses

---
+++ src/main/java/leecode200/LongestValidParentheses.java
@@ -0,0 +1,38 @@
+package leecode200;
+
+import java.util.Stack;
+
+/**
+ * @author sven
+ * @date 2022/09/05
+ * @desprication 32. Longest Valid Parentheses
+ */
+public class LongestValidParentheses {
+
+
+    public int longestValidParentheses1(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int[] dp = new int[s.length() + 1];
+        for (int i=0; i< s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+                dp[i+1] =0;
+            } else {
+                if (!stack.isEmpty()) {
+                    int leftIndex = stack.pop();
+                    int len = i - leftIndex + dp[leftIndex] + 1;
+                    dp[i+1] = len;
+                } else {
+                    dp[i+1] = 0;
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i=0; i < dp.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+
+    }
+}
