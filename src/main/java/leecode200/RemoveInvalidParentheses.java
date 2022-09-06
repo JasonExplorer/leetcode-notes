d0019c39deb37a3ce38f72aa5f8a0c3505af4a47
xixibb <liu_yichun@163.com>
2022-09-06 21:16:35 +0800

    fix

---
+++ src/main/java/leecode200/RemoveInvalidParentheses.java
@@ -0,0 +1,15 @@
+package leecode200;
+
+import java.util.List;
+
+/**
+ * @author sven
+ * @date 2022/09/05
+ * @desprication 301. Remove Invalid Parentheses
+ */
+public class RemoveInvalidParentheses {
+
+    public List<String> removeInvalidParentheses(String s) {
+        return null;
+    }
+}
---
+++ src/main/java/leecode200/SerializeAndDeserializeBinaryTree.java
@@ -0,0 +1,21 @@
+package leecode200;
+
+import common.TreeNode;
+
+/**
+ * @author sven
+ * @date 2022/09/04
+ * @desprication 297. Serialize and Deserialize Binary Tree
+ */
+public class SerializeAndDeserializeBinaryTree {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        return null;
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        return null;
+    }
+}
