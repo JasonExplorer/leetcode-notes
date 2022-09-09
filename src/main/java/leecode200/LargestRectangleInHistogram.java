9e7aeb32df4dac038c3a332ee2bf4b3b26649390
xixibb <liu_yichun@163.com>
2022-09-09 21:18:39 +0800

    84. Largest Rectangle in Histogram

---
+++ src/main/java/leecode200/LargestRectangleInHistogram.java
@@ -0,0 +1,89 @@
+package leecode200;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author xixibb
+ * @date 2022/09/09
+ * @desprication 84. Largest Rectangle in Histogram
+ */
+public class LargestRectangleInHistogram {
+
+    public int largestRectangleArea(int[] heights) {
+
+        int len = heights.length;
+        if (len == 0) {
+            return 0;
+        }
+
+        int res = 0;
+        for (int i =0;i < len;i++) {
+            int left = i;
+            int curHeight = heights[i];
+            //找左边最后 1 个大于等于 heights[i] 的下标
+            while (left>0 && heights[left -1] >= curHeight) {
+                left--;
+            }
+
+            // 找右边最后 1 个大于等于 heights[i] 的索引
+            int right = i;
+            while (right < len - 1 && heights[right + 1] >= curHeight) {
+                right++;
+            }
+
+            int width = right - left + 1;
+            res = Math.max(res, width * curHeight);
+        }
+        return res;
+    }
+
+    public int largestRectangleArea2(int[] heights) {
+
+        int len = heights.length;
+        if (len == 0) {
+            return 0;
+        }
+        if (len == 1) {
+            return heights[0];
+        }
+
+        int res = 0;
+        Deque<Integer> stack = new ArrayDeque<>(len);
+        for (int i = 0; i < len; i++) {
+            // 这个 while 很关键，因为有可能不止一个柱形的最大宽度可以被计算出来
+            while (!stack.isEmpty() && heights[i] < heights[stack.peekLast()]) {
+                int curHeight = heights[stack.pollLast()];
+                while (!stack.isEmpty() && heights[stack.peekLast()] == curHeight) {
+                    stack.pollLast();
+                }
+
+                int curWidth;
+                if (stack.isEmpty()) {
+                    curWidth = i;
+                } else {
+                    curWidth = i - stack.peekLast() - 1;
+                }
+
+                // System.out.println("curIndex = " + curIndex + " " + curHeight * curWidth);
+                res = Math.max(res, curHeight * curWidth);
+            }
+            stack.addLast(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int curHeight = heights[stack.pollLast()];
+            while (!stack.isEmpty() && heights[stack.peekLast()] == curHeight) {
+                stack.pollLast();
+            }
+            int curWidth;
+            if (stack.isEmpty()) {
+                curWidth = len;
+            } else {
+                curWidth = len - stack.peekLast() - 1;
+            }
+            res = Math.max(res, curHeight * curWidth);
+        }
+        return res;
+    }
+}
